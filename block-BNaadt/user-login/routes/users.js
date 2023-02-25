df77ca8c263aabe678a7d71bd21010b8ab92089d
ajay <ajayrajput9554@gmail.com>
2023-02-26 03:09:25 +0530

    auth flash & login block-3

---
+++ block-BNaadt/user-login/routes/users.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../model/User')
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  console.log(req.session)
+  res.render('user');
+});
+
+// registration
+router.get('/registration', function (req, res, next) {
+  var error = req.flash('error')[0]
+  res.render('registration', { error });
+});
+
+
+router.post('/registration', function (req, res, next) {
+  User.create(req.body, (err, user) => {
+
+    if (err) {
+
+      if (err.name === 'MongoServerError') {
+        req.flash('error', 'This email is taken')
+        return res.redirect('/users/registration')
+      }
+      if (err.name === 'ValidationError') {
+        req.flash('error', err.message)
+        return res.redirect('/users/registration')
+      }
+
+    }
+    res.redirect('/')
+  })
+});
+
+// login
+router.get('/login', function (req, res, next) {
+  var error = req.flash('error')[0]
+  res.render('login', { error });
+});
+
+
+router.post('/login', function (req, res, next) {
+  var { email, password } = req.body
+  if (!email || !password) {
+    req.flash('error', 'Please enter email/password')
+    return res.redirect('/users/login')
+  }
+
+
+  User.findOne({ email }, (err, user) => {
+    if (err) return next(err)
+    // no user
+    if (!user) {
+      req.flash('error', 'This email is not registered')
+      return res.redirect('/users/login')
+    }
+    // compare password
+    user.verifyPassword(password, (err, result) => {
+      if (err) return next(err)
+      // console.log(result)
+      if (!result) {
+        req.flash('error', 'Incorrect password')
+        return res.redirect('/users/login')
+      }
+      else {
+
+        // persist logged in user information
+        req.session.userId = user.id
+        res.redirect('/users/dashboard')
+      }
+    })
+  })
+});
+
+
+router.get('/dashboard', (req, res, next) => {
+  if (req.session.userId) {
+    return res.redirect('/users')
+  }
+
+  return res.redirect('/users/login')
+})
+
+
+// logout
+router.get('/logout', (req, res, next) => {
+  // res.clearCookie('connect.sid')
+  req.session.destroy()
+  res.redirect('/users/login')
+})
+
+module.exports = router;
